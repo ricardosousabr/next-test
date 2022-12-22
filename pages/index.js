2711a44787630a245f764f717bbaeb9a5120240f
ricardo <ricardo.br.pi@gmail.com>
2022-12-21 21:14:52 -0300

    style: text explaining about snapshot tests

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
       <div>
         <h1>tests in next</h1>
+        <p>Test snapshot: Testes de snapshot são ferramentas bem úteis sempre que você desejar garantir que sua UI não seja alterada inesperadamente.</p>
       </div>
   )
 }
