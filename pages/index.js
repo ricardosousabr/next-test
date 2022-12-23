17c21d0f7edba82082946080bc4c5229a5e36552
ricardo <ricardo.br.pi@gmail.com>
2022-12-22 21:11:33 -0300

    feat: create button component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import { Button } from "../src/components/Button"
+
 export default function Home() {
   return (
       <div>
         <h1>tests in next</h1>
         <p>Test snapshot: Testes de snapshot são ferramentas bem úteis sempre que você desejar garantir que sua UI não seja alterada inesperadamente.</p>
+        <Button>Click</Button>
       </div>
   )
 }
